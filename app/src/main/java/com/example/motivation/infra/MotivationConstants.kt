ffc4624d9e21e36436f275d63dc83a469b523bbd
Julio Alves dos Santos <ajulioeng@gmail.com>
2023-01-16 10:51:12 -0300

    Initial commit

---
+++ app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
@@ -0,0 +1,15 @@
+package com.example.motivation.infra
+
+class MotivationConstants private constructor() {
+
+    object KEY {
+        const val PERSON_NAME = "personName"
+    }
+
+    object PHRASEFILTER {
+        const val ALL = 0
+        const val HAPPY = 1
+        const val SUNNY = 2
+    }
+
+}
